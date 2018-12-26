724ed54798ec8c4a9d6ca26169a68edd1e4ce8ab
Dennis Wong <denniswong34@hotmail.com>
2018-12-26 23:27:37 +0800

    add exchange

--- src/lib/type/exchange.ts
+++ src/lib/type/exchange.ts
@@ -31,23 +31,60 @@ export enum ExchangeId {
   KuCoin = 'kucoin',
   Binance = 'binance',
   Bitbank = 'bitbank',
+  Bittrex = 'bittrex',
+  Cryptopia = 'cryptopia',
+  Hitbtc = 'hitbtc2',
+  Cobinhood = 'cobinhood',
+  Livecoin = 'livecoin',
+  Okex = 'okex',
+  Huobipro = 'huobipro',
+  Poloniex = 'poloniex',
 }
 
 export const SupportExchanges = [
   {
     id: ExchangeId.KuCoin,
-    name: '库币',
+    name: 'Kucoin',
   },
   {
     id: ExchangeId.Binance,
-    name: '币安',
+    name: 'Binance',
   },
   {
     id: ExchangeId.Bitbank,
     name: 'Bitbank',
   },
-];
-
+  {
+      id: ExchangeId.Bittrex,
+      name: 'Bittrex',
+  },
+  {
+      id: ExchangeId.Cryptopia,
+      name: 'Cryptopia',
+  },
+  {
+      id: ExchangeId.Cobinhood,
+      name: 'Cobinhood',
+  },
+  {
+      id: ExchangeId.Hitbtc,
+      name: 'Hitbtc',
+  },
+  {
+      id: ExchangeId.Livecoin,
+      name: 'Livecoin',
+  },
+  {
+      id: ExchangeId.Okex,
+      name: 'Okex',
+  },
+  {
+        id: ExchangeId.Huobipro,
+        name: 'Huobipro',
+  },
+  {
+        id: ExchangeId.Poloniex,
+  },
 export interface ICredentials {
   apiKey: string;
   secret: string;

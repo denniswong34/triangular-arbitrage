595aceaae914a16b133b66f8d93eaa05ea84d2c1
Dennis Wong <denniswong34@hotmail.com>
2018-12-29 08:30:08 +0800

    debug

--- src/lib/common/helper.ts
+++ src/lib/common/helper.ts
@@ -243,11 +243,14 @@ export class Helper {
     logger.debug(`Symbol: ` + JSON.stringify(symbol, null, 2));
 
     const precision = symbol.precision.amount ? symbol.precision.amount : 8;
+    const selfCalCost = (((symbol.limits.price.min ? symbol.limits.amount.min * 2 :
+        symbol.limits.amount.min * symbol.info.low)).toFixed(precision));
 
     return {
       amount: symbol.precision.amount,
       price: symbol.precision.price,
-      cost: parseFloat(((symbol.limits.price.min ? symbol.limits.amount.min * 2 : symbol.limits.amount.min * symbol.info.low)).toFixed(precision)),
+      cost: symbol.limits.cost ? symbol.limits.cost.min ? symbol.limits.cost.min :
+          selfCalCost : selfCalCost,
     };
   }
 

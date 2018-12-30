161313ab68295862f5c74fda59ba0a90ac27bb0f
Dennis Wong <denniswong34@hotmail.com>
2018-12-30 23:55:50 +0800

    debug

--- src/lib/api-handler.ts
+++ src/lib/api-handler.ts
@@ -11,7 +11,7 @@ const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 const specialTicker = ['NZDT/USD'];
 
-const cmcConvertedTicker: any = {'USD/USD': 'USDT/USD'};
+const cmcConvertedTicker: any = {'USD/USD': 'USDT/USD', 'EUR/USD': 'EURT/USD'}
 
 let createOrderFailCount : number = 0;
 
